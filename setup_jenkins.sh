77e424aea42fb3137318b04fdfef192dd89c8d01
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-08 02:35:46 +0530

    Update setup_jenkins.sh

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -9,7 +9,6 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debi
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install jenkins -y
-java -jar /usr/share/jenkins/jenkins.war --version  # Verify Jenkins version
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
