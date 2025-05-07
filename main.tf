d31344802fc9ef538672dd31e1c328e34f67fb03
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-07 23:29:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "jenkins" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = "gp3"
+  }
+
+  user_data = file("${path.module}/setup_jenkins.sh")
+
+  tags = {
+    Name = "Jenkins-Machine"
+  }
+}
