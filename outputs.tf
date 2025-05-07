5f2bc081e7dd87663069db145425bacb19195c40
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-08 04:21:24 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "jenkins_instance_id" {
   description = "Instance ID of the Jenkins EC2"
   value       = aws_instance.jenkins.id
 }
-
-output "jenkins_password_s3" {
-  description = "Jenkins password stored in S3"
-  value       = "s3://terraform-backend-faisal-khan/jenkins_password.txt"
-  sensitive   = true
-}
