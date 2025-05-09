859fb3c066a32e135e9c728d41eb512fc37a6d4e
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-10 02:33:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "jenkins" {
+resource "aws_instance" "jenkins_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
