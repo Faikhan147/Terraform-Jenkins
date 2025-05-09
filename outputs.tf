560ccc4e1b36d7dc984aa7b62b3b3e7dbfa5dad3
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-10 01:11:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "jenkins_public_ip" {
   description = "Public IP of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins.public_ip
+  value       = aws_instance.setup_jenkins.public_ip
 }
 
 output "jenkins_instance_id" {
   description = "Instance ID of the Jenkins EC2"
-  value       = aws_instance.jenkins.id
+  value       = aws_instance.setup_jenkins.id
 }
