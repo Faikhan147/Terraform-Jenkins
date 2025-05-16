cabb277275ee742384f7e36f2868a953ccccaceb
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-16 23:58:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "jenkins_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = var.key_name
+  #key_name               = var.key_name
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = "jenkins-s3-access"
 
