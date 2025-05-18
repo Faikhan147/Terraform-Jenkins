1b6a9af8ddc2eb07a4788d7ff8b0aa32aecd8d0f
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 16:09:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,7 @@ resource "aws_instance" "jenkins_instance" {
   instance_type          = var.instance_type
   #key_name               = var.key_name
   vpc_security_group_ids = [var.security_group_id]
-  iam_instance_profile = aws_iam_instance_profile.jenkins_s3_profile.name
-  iam_instance_profile = aws_iam_instance_profile.jenkins_ssm_profile.name
-
+  iam_instance_profile = aws_iam_instance_profile.jenkins_s3_ssm_profile.name
 
   root_block_device {
     volume_size = var.volume_size
