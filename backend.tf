8937d17a937c0a07e031e2cad64ae58cf188dd0d
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-21 01:52:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "terraform-backend-faisal-khan"
     key            = "jenkins/terraform.tfstate"
     region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
