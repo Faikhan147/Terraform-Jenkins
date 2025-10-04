64a8365b6780d98cebffcd0d3ab2bf369f67e89a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 13:32:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,22 +4,52 @@ variable "region" {
 }
 
 
+variable "instance_profile_name" {
+  description = "Name of the IAM Instance Profile to attach to the EC2 instance"
+  type        = string
+}
+
+variable "jenkins_role_name" {
+  description = "Name of the existing Jenkins IAM Role"
+  type        = string
+}
+
+variable "template_name" {
+  description = "Name prefix for the Jenkins Launch Template"
+  type        = string
+}
+
 variable "ami_id" {
-  description = "AMI ID for the Jenkins instance"
+  description = "AMI ID for the Jenkins EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "EC2 instance type for Jenkins server"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "VPC Security Group ID to attach to the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be launched"
   type        = string
 }
 
 variable "volume_size" {
-  description = "Size of the root EBS volume"
+  description = "Root EBS volume size in GB"
   type        = number
 }
 
 variable "volume_type" {
-  description = "Type of the root EBS volume"
+  description = "Root EBS volume type (e.g., gp3, gp2, io1)"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
   type        = string
 }
