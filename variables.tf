33cf1916596f74d231a457e9c82c2c5ac7dcbf08
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 12:22:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "instance_type" {
   type        = string
 }
 
-variable "security_group_id" {
-  description = "Security Group ID"
-  type        = string
-}
-
 variable "volume_size" {
   description = "Size of the root EBS volume"
   type        = number
@@ -20,30 +15,5 @@ variable "volume_size" {
 
 variable "volume_type" {
   description = "Type of the root EBS volume"
-  type        = string
-}
-
-variable "jenkins_s3_ssm_role_name" {
-  description = "IAM Role name for Jenkins s3 access"
-  type        = string
-}
-
-variable "kms_key_arn" {
-  description = "ARN of the KMS key to allow decrypt access"
-  type        = string
-}
-
-variable "kms_key_name" {
-  description = "KMS key name"
-  type        = string
-}
-
-variable "AmazonS3FullAccess_arn" {
-  description = "ARN of the KMS key to allow s3 decrypt access"
-  type        = string
-}
-
-variable "AmazonSSMManagedInstanceCore_arn" {
-  description = "ARN of the policy to allow SSM access"
   type        = string
 }
